1c830340be9891c581cf0b334011b9923db5b64b
okj4321 <okj4321@gmail.com>
2019-08-01 16:19:04 +0900

    second commit

--- voteproject/settings.py
+++ voteproject/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nz+9h*mini0drdda!aqwpj$ldghl_^4emwhu92im_q5y3sz(!='
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'nz+9h*mini0drdda!aqwpj$ldghl_^4emwhu92im_q5y3sz(!=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
